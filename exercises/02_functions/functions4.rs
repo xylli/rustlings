b66c42fd4a36699b96aeaa598eb19537680d08ee
xylli <peter.suepfle@gmail.com>
2023-11-01 22:00:50 +0100

    functions

--- exercises/02_functions/functions4.rs
+++ exercises/02_functions/functions4.rs
@@ -8,15 +8,13 @@
 // Execute `rustlings hint functions4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let original_price = 51;
     println!("Your sale price is {}", sale_price(original_price));
 }
 
-fn sale_price(price: i32) -> {
-    if is_even(price) {
+fn sale_price(price: i32) -> i32{
+    return if is_even(price) {
         price - 10
     } else {
         price - 3
