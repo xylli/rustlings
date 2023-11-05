3c6b3de53a5421b88b4806e231d6a29bf138816a
xylli <peter.suepfle@gmail.com>
2023-11-05 19:52:03 +0100

    ifs & primitives

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: i32) -> i32 {
+    return if amount <= 40 {
+        2 * amount
+    } else {
+        amount
+    }
+}
 
 // Don't modify this function!
 #[test]
